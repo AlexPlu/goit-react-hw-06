0aea8b212e46d74f92b3428773c47b0221d35f29
Oleksandr Pluzhnykov <bbynnas@yahoo.com>
2024-06-06 12:44:13 +0300

    Move Provider and PersistGate to main

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,3 @@
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-import { store, persistor } from "../../redux/store";
 import ContactForm from "../ContactForm/ContactForm";
 import ContactList from "../ContactList/ContactList";
 import SearchBox from "../SearchBox/SearchBox";
@@ -8,16 +5,12 @@ import "./App.css";
 
 function App() {
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <div>
-          <h1>Phonebook</h1>
-          <ContactForm />
-          <SearchBox />
-          <ContactList />
-        </div>
-      </PersistGate>
-    </Provider>
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <SearchBox />
+      <ContactList />
+    </div>
   );
 }
 
